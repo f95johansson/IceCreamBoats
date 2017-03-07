c255d762e26b704931ba70978d1f87a7ee160984
Fredrik Johansson <f95johansson@gmail.com>
2017-03-07 19:29:54 +0100

    Imported Airbnb map component

--- index.android.js
+++ index.android.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import * as firebase from 'firebase'
+import * as firebase from 'firebase';
+import MapView from 'react-native-maps';
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View
-} from 'react-native'
+} from 'react-native';
 
 // Initialize Firebase
 var config = {
@@ -14,15 +15,17 @@ var config = {
   databaseURL: "https://icecreamboats-6da6d.firebaseio.com",
   storageBucket: "icecreamboats-6da6d.appspot.com",
   messagingSenderId: "873538806157"
-}
-firebase.initializeApp(config)
+};
+firebase.initializeApp(config);
+
+
 
 export default class IceCreamBoats extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+        {/*<Text style={styles.welcome}>
+          Welcome to React Native Hello!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
@@ -30,19 +33,27 @@ export default class IceCreamBoats extends Component {
         <Text style={styles.instructions}>
           Double tap R on your keyboard to reload,{'\n'}
           Shake or press menu button for dev menu
-        </Text>
+        </Text>*/}
+        <MapView style={styles.map}
+          region={{
+            latitude: 57.653263,
+            longitude: 11.777580,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  /*container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
+  },*/
   welcome: {
     fontSize: 20,
     textAlign: 'center',
@@ -53,6 +64,16 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  container: {
+    // jshint ignore:start
+   ...StyleSheet.absoluteFillObject,
+   justifyContent: 'flex-end',
+   alignItems: 'center',
+   },
+   map: {
+     ...StyleSheet.absoluteFillObject,
+   // jshint ignore:end
+   },
 });
 
 AppRegistry.registerComponent('IceCreamBoats', () => IceCreamBoats);
