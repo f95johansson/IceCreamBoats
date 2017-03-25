8510b2697de6841f63b45bb4bb796196a5731948
thelitlej <jonasgustafson@hotmail.se>
2017-03-25 17:39:17 +0100

    Uninstalled all the nonused packages. Fixed zoom in menu

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,25 +1,32 @@
-import Image from 'react-native-transformable-image';
+//import Image from 'react-native-transformable-image';
 import React, { Component } from 'react';
 import * as firebase from 'firebase';
 import MapView from 'react-native-maps';
 import {
+  Dimensions,
   StyleSheet,
   Text,
   View,
+  Image
 } from 'react-native';
+import ImageZoom from 'react-native-image-pan-zoom';
 
-export default class IceCreamBoats extends Component {
+export default class Menu extends Component {
   render() {
     return (
       <View>
-          <Image
-          style={{height:515}}
-          source={{uri: 'http://freshcoast.se/wp-content/uploads/2016/06/meny_2016.jpg'}}
-          enableTranslate = {false}
-          //pixels={{width: 3607, height: 2400}}
-        />
-       
+        <ImageZoom 
+          cropWidth={Dimensions.get('window').width}
+          cropHeight={Dimensions.get('window').height-100}
+          imageWidth={Dimensions.get('window').width}
+          imageHeight={583}
+          longPressTime={100}>
+          <Image 
+            style={{width:Dimensions.get('window').width, height:550}}
+            source={{uri:'http://freshcoast.se/wp-content/uploads/2016/06/meny_2016.jpg'}}/>
+        </ImageZoom>        
       </View>
     )
   }
 }
+
