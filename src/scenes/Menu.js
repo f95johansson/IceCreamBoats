704008f14080b5aec602aedc77adfd9fd675bd91
thelitlej <jonasgustafson@hotmail.se>
2017-03-25 15:32:43 +0100

    menu image added with zoom

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,3 +1,4 @@
+import Image from 'react-native-transformable-image';
 import React, { Component } from 'react';
 import * as firebase from 'firebase';
 import MapView from 'react-native-maps';
@@ -5,16 +6,19 @@ import {
   StyleSheet,
   Text,
   View,
-  Image
 } from 'react-native';
 
 export default class IceCreamBoats extends Component {
   render() {
     return (
       <View>
-        {/*Import ImageViewer Here
-          https://facebook.github.io/react-native/docs/images.html
-          */}
+          <Image
+          style={{height:515}}
+          source={{uri: 'http://freshcoast.se/wp-content/uploads/2016/06/meny_2016.jpg'}}
+          enableTranslate = {false}
+          //pixels={{width: 3607, height: 2400}}
+        />
+       
       </View>
     )
   }
