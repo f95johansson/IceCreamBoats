42d52919c5f56832fa1e7c23e0425e6761b4c844
syltman <mcoquand@gmail.com>
2017-03-25 15:21:05 +0100

    Implemented splashscreen for Android

--- android/app/src/main/java/com/icecreamboats/MainActivity.java
+++ android/app/src/main/java/com/icecreamboats/MainActivity.java
@@ -2,8 +2,13 @@ package com.icecreamboats;
 
 import com.facebook.react.ReactActivity;
 
+import com.cboy.rn.splashscreen.SplashScreen;
 public class MainActivity extends ReactActivity {
-
+   @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
