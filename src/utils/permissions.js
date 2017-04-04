d38d4cc3bb8c811e8b4ca39b61ab303389f2f781
Fredrik Johansson <f95johansson@gmail.com>
2017-04-04 09:54:14 +0200

    Adding a file I forgot to add

---
+++ src/utils/permissions.js
@@ -0,0 +1,19 @@
+export async function requestCameraPermission() {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.CAMERA,
+      {
+        'title': 'Cool Photo App Camera Permission',
+        'message': 'Cool Photo App needs access to your camera ' +
+                   'so you can take awesome pictures.'
+      }
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log("You can use the camera");
+    } else {
+      console.log("Camera permission denied");
+    }
+  } catch (err) {
+    console.warn(err);
+  }
+}
