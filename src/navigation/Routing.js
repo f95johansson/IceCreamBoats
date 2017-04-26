b929561db512b8788113d54a121d54292bfde0d0
syltman <mcoquand@gmail.com>
2017-04-26 14:31:47 +0200

    glhf fredrik

--- src/navigation/Routing.js
+++ src/navigation/Routing.js
@@ -27,20 +27,45 @@ export default class Routing extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {page:'Karta'};
+    this.state = {
+      page:'Karta',
+      routes :{
+        Info: {
+          scene: <About openAdmin={this.openAdmin}/>, 
+          title: 'Info',
+          index: INDEX.ABOUT,
+          iconSelected: require('../../assets/tabbar/infoSelected/infoSelected.png'),
+          icon: require('../../assets/tabbar/info/info.png'),
+          show: false,
+        },
+        Karta: {
+          scene: <MapScene />, 
+          title: 'Karta',   
+          index: INDEX.MAP,
+          iconSelected: require('../../assets/tabbar/mapSelected/mapSelected.png'),
+          icon: require('../../assets/tabbar/map/map.png'),
+          show: true,
+        },
+        Utbud: {
+          scene: <Menu />,     
+          title: 'Utbud',   
+          index: INDEX.MENU,
+          iconSelected: require('../../assets/tabbar/menuSelected/menuSelected.png'),
+          icon: require('../../assets/tabbar/menu/menu.png'),
+          show: false,
+        },
+        Admin: {
+          scene: <Admin />,    
+          title: 'Admin',   
+          index: INDEX.ADMIN,
+          iconSelected: require('../../assets/tabbar/infoSelected/infoSelected.png'), 
+          icon: require('../../assets/tabbar/info/info.png'),
+          show: false,
+        },
+      },
+    }
     this.openAdmin = this.openAdmin.bind(this);
   
-    this.routes = {
-      Info: {scene: <About openAdmin={this.openAdmin} />, title: 'Info',  index: INDEX.ABOUT,
-        iconSelected: require('../../assets/tabbar/infoSelected/infoSelected.png'), icon: require('../../assets/tabbar/info/info.png')},
-      Karta: {scene: <MapScene />, title: 'Karta',   index: INDEX.MAP,
-        iconSelected: require('../../assets/tabbar/mapSelected/mapSelected.png'), icon: require('../../assets/tabbar/map/map.png')},
-      Utbud: {scene: <Menu />,     title: 'Utbud',   index: INDEX.MENU,
-        iconSelected: require('../../assets/tabbar/menuSelected/menuSelected.png'), icon: require('../../assets/tabbar/menu/menu.png')},
-      Admin: {scene: <Admin />,    title: 'Admin',   index: INDEX.ADMIN,
-       iconSelected: require('../../assets/tabbar/infoSelected/infoSelected.png'), icon: require('../../assets/tabbar/info/info.png')},
-    };
-
     this.setAndroidBackPressButton();
   }
 
@@ -59,26 +84,54 @@ export default class Routing extends Component {
     this.setState({page: 'Admin'})
   }
 
+  changeView(newPage) {
+    var newState = this.state.routes;
+    // Hide old page
+    // Set page to new page and show it
+    newState[this.state.page].show = false;
+    this.setState({page:newPage.props.name})
+    newState[this.state.page].show = true;
+    this.setState({routes: newState})
+  }
+
+  renderSinglePage(name, index){
+    return (
+          <View style={[styles.Scene, this.state.routes[name].show ? []: {width:0, height:0}]}>
+          {this.state.routes[name].scene}
+          </View>
+        )
+  }
+
+  renderPages(){
+    // Loop through routes and render each
+      return (Object.keys(this.state.routes).map((name, index) => {
+       this.renderSinglePage(name, index);
+      }))
+    }
+
   render() {
     return (
       <View style={styles.Routing}>
-        {<View style={styles.Scene}>{this.routes[this.state.page].scene}</View>}
-        <Tabs selected={this.state.page} style={{backgroundColor:'white'}}
-              selectedStyle={{selected: true}} onSelect={el=>this.setState({page:el.props.name})}>
+        {this.renderPages()}
+        <Tabs selected={this.state.page} 
+              style={{backgroundColor:'white'}}
+              selectedStyle={{selected: true}} 
+              onSelect={el=>this.changeView(el)}>
 
-            {Object.keys(this.routes).map((name, index) => {
+            {Object.keys(this.state.routes).map((name, index) => {
               if (name !== 'Admin') {
                 return <TabButton 
-                  name={this.routes[name].title} key={this.routes[name].title} route={this.routes[name]} styles={styles}
+                  name={this.state.routes[name].title} 
+                  key={this.state.routes[name].title} 
+                  route={this.state.routes[name]} 
+                  styles={styles}
                 />}
               })
             }
         </Tabs>
       </View>
     );
-
   }
-
 }
 
 function TabButton (props) {
